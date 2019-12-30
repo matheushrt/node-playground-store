81a672dd5982a61b29a6f77c72f6d82896dfb842
Matheus Souza <matheushrt@gmail.com>
2019-12-30 19:52:47 -0300

    created auth middleware

---
+++ controllers/authController.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+
+exports.login = passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: 'Failed Login!',
+  successRedirect: '/',
+  successFlash: 'You are now logged in!'
+});
+
+exports.logout = (req, res) => {
+  console.log(req.logout);
+
+  req.logout();
+  req.flash('success', 'You are now logged out!');
+  res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+    return;
+  }
+  req.flash('error', 'Oops you must be logged in to do that!');
+  res.redirect('/login');
+};

e5b3dbcc3596db0f22a077fe9bb2df924e5e1028
Matheus Souza <matheushrt@gmail.com>
2019-12-31 13:04:49 -0300

    added account edittion and password reset routes

--- routes/index.js
+++ routes/index.js
@@ -34,5 +34,14 @@ router.post(
 );
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
+router.post('/account/forgot', catchErrors(authController.forgot));
+router.get('/account/reset/:token', catchErrors(authController.reset));
+router.post(
+  '/account/reset/:token',
+  authController.confirmedPasswords,
+  catchErrors(authController.reset)
+);
 
 module.exports = router;
